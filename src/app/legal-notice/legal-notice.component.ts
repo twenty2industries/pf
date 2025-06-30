bc93c665ea290ade186b63d6417d5df8002a2df5
twenty2industries <burak.bostanli.1@web.de>
2025-06-30 18:02:13 +0200

    feat: legal-notice.components almost done, need to relocate header for legal-notice

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
+import {TranslatePipe, TranslateDirective} from "@ngx-translate/core";
+
+
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [],
+  imports: [HeaderComponent, TranslatePipe, TranslateDirective],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
